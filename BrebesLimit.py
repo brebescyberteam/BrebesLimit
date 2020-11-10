6a70ec6c7a4ed9818ad53d7542b620ab76e2dec1
PEMULUNG ELITE INDONESIA <52577203+brebescyberteam@users.noreply.github.com>
2020-11-11 01:46:40 +0700

    Update BrebesLimit.py

--- BrebesLimit.py
+++ BrebesLimit.py
@@ -9,7 +9,7 @@ sys.setdefaultencoding('utf8')
 br = mechanize.Browser()
 br.set_handle_robots(False)
 br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
-br.addheaders = [('User-Agent', 'Opera/9.80 (Android; Opera Mini/32.0.2254/85. U; id) Presto/2.12.423 Version/12.16')]
+br.addheaders = [('User-Agent', 'Mozilla/5.0 (Linux; U; Android 8.1.0; vivo 1807 Build/OPM1.171019.026; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/86.0.4240.185 Mobile Safari/537.36 OPR/52.1.2254.54298')]
 
 def keluar():
     print '\x1b[1;91m[!] Metu'
